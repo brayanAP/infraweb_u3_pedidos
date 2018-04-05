74da61ddb4760dbd3dbcaf2d892d84525ebf2223
brayanAP <brulariaspe@gmail.com>
2018-04-04 23:12:31 -0500

    brayanAP
    Creacion modelo de pedido

---
+++ models/Pedido.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%pedido}}".
+ *
+ * @property int $id
+ * @property string $fecha
+ * @property int $clienteid
+ *
+ * @property Cliente $cliente
+ */
+class Pedido extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%pedido}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['fecha', 'clienteid'], 'required'],
+            [['fecha'], 'safe'],
+            [['clienteid'], 'integer'],
+            [['clienteid'], 'exist', 'skipOnError' => true, 'targetClass' => Cliente::className(), 'targetAttribute' => ['clienteid' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'fecha' => Yii::t('app', 'Fecha'),
+            'clienteid' => Yii::t('app', 'Clienteid'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCliente()
+    {
+        return $this->hasOne(Cliente::className(), ['id' => 'clienteid']);
+    }
+
+    /**
+     * @inheritdoc
+     * @return PedidoQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new PedidoQuery(get_called_class());
+    }
+}
