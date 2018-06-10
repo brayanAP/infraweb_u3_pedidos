4a730fb55a241d69b64b5024cd8a54cf11e9134d
brayanAP <brulariaspe@gmail.com>
2018-06-10 16:04:07 -0600

    brayanAP

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=pedidos',
+    'dsn' => 'mysql:host=localhost;dbname=pedidoss',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
